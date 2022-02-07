8ae413aeba739724ea8068a20e205e3dfe579ac4
mayconline <mayconline.ti@gmail.com>
2022-02-07 11:55:55 -0300

    fixing import error jesst

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   clearMocks: true,
   collectCoverage: false,
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/**/**/*.spec.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  resolver: 'jest-ts-webcompat-resolver',
 };
